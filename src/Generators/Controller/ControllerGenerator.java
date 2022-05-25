c0dedb2df46a8efb45cd34fa672cf963da8fcab0
SalemAlashour2 <90504661+SalemAlashour2@users.noreply.github.com>
2022-05-25 21:53:45 +0300

    if statement

---
+++ src/Generators/Controller/ControllerGenerator.java
@@ -0,0 +1,37 @@
+package Generators.Controller;
+
+import Models.ControllerModels.Controller;
+import Models.ControllerModels.ControllerTokens;
+
+public class ControllerGenerator {
+
+    StringBuilder stringBuilder = new StringBuilder();
+    IfStatementGenerator ifStatementGenerator;
+    AssignGenerator assignGenerator;
+
+
+    public String generateController(Controller controller) {
+
+        if (controller.getControllerTokens().size() > 0) {
+            for (int i = 0; i < controller.getControllerTokens().size(); i++) {
+               generateControllerToken(controller.getControllerTokens().get(i));
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public String generateControllerToken(ControllerTokens controllerTokens)
+    {
+        if (controllerTokens.getIfStatement() != null) {
+            ifStatementGenerator = new IfStatementGenerator();
+            return  ifStatementGenerator.ifStatementGenerate(controllerTokens.getIfStatement());
+        }
+
+        if (controllerTokens.getAssign() != null){
+            assignGenerator = new AssignGenerator();
+            return assignGenerator.generateAssign(controllerTokens.getAssign());
+        }
+    }
+
+}
