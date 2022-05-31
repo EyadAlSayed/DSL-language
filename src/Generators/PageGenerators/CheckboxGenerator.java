fb8dd9194c0d1677f9f6ffd5cc7d4948c566761e
Caesar <caesarfarah@yahoo.com>
2022-05-31 12:14:01 +0300

    body attributes generators.

--- src/Generators/PageGenerators/CheckboxGenerator.java
+++ src/Generators/PageGenerators/CheckboxGenerator.java
@@ -7,8 +7,14 @@ public class CheckboxGenerator {
     StringBuilder stringBuilder = new StringBuilder();
 
     public StringBuilder generateCheckbox(Checkbox checkbox) {
-        //append checkbox format.
-        // return stringBuilder;
-        return null;
+        for (int i = 0; i < checkbox.getCheckboxAttributes().size(); i++) {
+            stringBuilder.append("<label> ");
+            stringBuilder.append("<input type=").append("\"checkbox\" ");
+            if (checkbox.getNAME() != null) {
+                stringBuilder.append("name=\"").append(checkbox.getNAME()).append("\">");
+            }
+            stringBuilder.append(checkbox.getCheckboxAttributes().get(i)).append("</label>\n");
+        }
+        return stringBuilder;
     }
 }
