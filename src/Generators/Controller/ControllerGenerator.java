dd0afec02bc7e38003fd75cc913c24b675c13fc0
EYAD <eyadan.900@gmail.com>
2022-06-09 08:07:07 +0300

    adding send and print action

--- src/Generators/Controller/ControllerGenerator.java
+++ src/Generators/Controller/ControllerGenerator.java
@@ -8,30 +8,40 @@ public class ControllerGenerator {
     StringBuilder stringBuilder = new StringBuilder();
     IfStatementGenerator ifStatementGenerator;
     AssignGenerator assignGenerator;
+    PrintGenerator printGenerator;
+    SendGenerator sendGenerator;
 
 
     public String generateController(Controller controller) {
 
         if (controller.getControllerTokens().size() > 0) {
             for (int i = 0; i < controller.getControllerTokens().size(); i++) {
-               stringBuilder.append(generateControllerToken(controller.getControllerTokens().get(i)));
+                stringBuilder.append(generateControllerToken(controller.getControllerTokens().get(i)));
             }
         }
 
-      //  System.out.println(stringBuilder.toString());
+        //  System.out.println(stringBuilder.toString());
         return stringBuilder.toString();
     }
 
-    public String generateControllerToken(ControllerTokens controllerTokens)
-    {
+    public String generateControllerToken(ControllerTokens controllerTokens) {
         if (controllerTokens.getIfStatement() != null) {
             ifStatementGenerator = new IfStatementGenerator();
-            return  ifStatementGenerator.ifStatementGenerate(controllerTokens.getIfStatement());
+            return ifStatementGenerator.ifStatementGenerate(controllerTokens.getIfStatement());
         }
 
-        if (controllerTokens.getAssign() != null){
+        if (controllerTokens.getAssign() != null) {
             assignGenerator = new AssignGenerator();
             return assignGenerator.generateAssign(controllerTokens.getAssign());
+        }
+
+        if (controllerTokens.getPrint() != null) {
+            printGenerator = new PrintGenerator();
+            return printGenerator.generatePrint(controllerTokens.getPrint());
+        }
+        if (controllerTokens.getSend() != null){
+            sendGenerator = new SendGenerator();
+            return sendGenerator.sendGenerator(controllerTokens.getSend());
         }
         return "\n";
     }
